4910caf647736be32574643cad51f1d3fc0d7fe9
Ricardo Costa <racc.costa@gmail.com>
2017-08-06 22:42:36 -0300

    Login page

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+
+  { 
+    path: '', loadChildren: 'app/login/login.module#LoginModule' },
   {
-    path: '', loadChildren: 'app/login/login.module#LoginModule'
+    path: 'main', loadChildren: 'app/main/main.module#MainModule'
   }
 ];
 
